52748529f80fd1a1b8422888d1462f3d4301c152
bcmbf14 <bcmbf14@gmail.com>
2020-10-26 22:20:00 +0900

    Initial Commit

---
+++ StudySwiftUI/StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by jc.kim on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
